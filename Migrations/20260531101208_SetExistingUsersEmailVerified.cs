2029bb165374ef0e05ff7c642de8694e58cf7a37
richard hannah <richard.hannah1974@gmail.com>
2026-05-31 11:13:58 +0100

    migration to verify any existing accounts

---
+++ Migrations/20260531101208_SetExistingUsersEmailVerified.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoMailerBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetExistingUsersEmailVerified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                UPDATE "Users"
+                SET "EmailVerified" = true
+                WHERE "EmailVerificationToken" IS NULL
+                  AND "EmailVerified" = false
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
